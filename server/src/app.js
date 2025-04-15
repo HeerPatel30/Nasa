fd971fc5b7d488e345758ab2008928f0dcfafa56
Heer Patel <108482463+HeerPatel30@users.noreply.github.com>
2025-04-15 20:43:58 +0530

    final changes

--- server/src/app.js
+++ server/src/app.js
@@ -1,23 +1,24 @@
-import express from "express";
-import planetrouter from "./routes/planet.router.js";
-import cors from "cors";
-import morgan from "morgan"
-import launchesrouter from "./routes/launches.router.js";
+const path = require('path');
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const api = require('./routes/api');
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin: 'http://localhost:3000',
+}));
+app.use(morgan('combined'));
 
 app.use(express.json());
-app.use(morgan('combined'))
-app.use(planetrouter);
-app.use(launchesrouter)
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
-export default app;
+app.use('/v1', api);
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
+
+module.exports = app;
